e09b5fcdae147b72fbe2926a5e76fa70b9f25494
lixandro96 <80864384+lixandro96@users.noreply.github.com>
2023-01-23 22:11:57 -0400

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,9 @@
+
+const array = ["Juan","martin","Rosa","Stephanie"];
+const familia = new Set(array);
+
+familia.add("Lixandro");
+console.log(familia)
+
+familia.add("Javascript");
+console.log(familia)
